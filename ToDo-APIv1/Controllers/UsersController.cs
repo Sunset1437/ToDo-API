5eec49130337f9f4fde93a5a584e56aa2bac8fc5
KazeRaze <101569387+Sunset1437@users.noreply.github.com>
2024-08-07 16:55:05 +0300

    First WebAPI commit
    First WebAPI commit

---
+++ ToDo-APIv1/Controllers/UsersController.cs
@@ -0,0 +1,30 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using ToDo_APIv1.Models;
+
+namespace ToDo_APIv1.Controllers
+{
+    [ApiController,Route("api/[controller]")]
+    public class UsersController : ControllerBase
+    {
+        [HttpGet]
+        public void GetUserById(int id)
+        {
+
+        }
+        [HttpPost]
+        public void AddUser(UserModel user)
+        {
+
+        }
+        [HttpPut]
+        public void UpdateUser(UserModel user) 
+        { 
+        
+        }
+        [HttpDelete("{id}")]
+        public void DeleteUser(int id) 
+        { 
+        
+        }
+    }
+}
